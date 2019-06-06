fce4ec1e89286232f6f328f0db352134d3554fa1
LloydNicholson <nichlloyd@gmail.com>
2019-06-06 10:29:37 +0200

    layout changes to header component and margins changed to fit width and height correctly

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,18 +3,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles:
-      [`
-           a, h1 {
-               text-decoration: none;
-               margin-right: 10px;
-               color: white;
-           }
-
-           button {
-               margin: 5px;
-           }
-       `]
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   @Output() toggledNav = new EventEmitter<boolean>();
