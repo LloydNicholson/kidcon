28e5b0b408299a1befd6d0316827eadc2d239815
Lloyd Nicholson <nichlloyd@gmail.com>
2020-04-19 12:21:57 +0200

    Test app blazor preview 4

---
+++ Preview4/BlazorHostedPreview4/Shared/Models/ReceivedAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClientApp.Shared.Models
+{
+    public class ReceivedAccount
+    {
+        public string name { get; set; }
+        public List<string> alts { get; set; }
+        public string increasingSide { get; set; }
+        public string type { get; set; }
+    }
+}
