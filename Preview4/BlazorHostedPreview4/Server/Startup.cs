d90414668d5042ae5d389d4258fb888af392873c
Lloyd Nicholson <nichlloyd@gmail.com>
2020-04-19 12:37:08 +0200

    Remove entity framework

--- Preview4/BlazorHostedPreview4/Server/Startup.cs
+++ Preview4/BlazorHostedPreview4/Server/Startup.cs
@@ -23,7 +23,7 @@ namespace BlazorHostedPreview4.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<KidConDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<KidConDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews();
         }
