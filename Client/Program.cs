b73ea6f8c6bcf79ed57397f4de55d03950675a82
lloyd <nichlloyd@gmail.com>
2020-11-17 11:49:11 +0200

    TransactionBase class addition

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,8 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using ClientApp.Client.Services;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
-using Syncfusion.Blazor;
-using Syncfusion.Licensing;
 
 namespace ClientApp.Client
 {
@@ -19,8 +17,8 @@ namespace ClientApp.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddSyncfusionBlazor();
-            SyncfusionLicenseProvider.RegisterLicense("MjY4ODE4QDMxMzgyZTMxMmUzMENQVmgyNVMrenZmZ2ZIb0ZqaE53M1AzVk9qV01hbHpMUGR4VkV2RlVrZEk9");
+            //builder.Services.AddSyncfusionBlazor();
+            //SyncfusionLicenseProvider.RegisterLicense("MjY4ODE4QDMxMzgyZTMxMmUzMENQVmgyNVMrenZmZ2ZIb0ZqaE53M1AzVk9qV01hbHpMUGR4VkV2RlVrZEk9");
             builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             ConfigureServices(builder.Services);
 
